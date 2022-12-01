878d9a1858500178f40091ce0a01f8820e42512a
lu1sd4 <luisdaniel.ld24@gmail.com>
2022-12-01 22:48:00 +0100

    Initial commit

---
+++ 2022/day1/src/main.rs
@@ -0,0 +1,39 @@
+use std::cmp::max;
+use std::collections::BTreeSet;
+
+fn main() {
+    let file_contents = include_str!("input");
+    part_one(file_contents); 
+    println!();
+    part_two(file_contents);
+}
+
+fn part_one(file_contents: &str) {
+    let mut max_cals: u32 = 0;
+    let mut current_cals: u32 = 0;
+    for line in file_contents.lines() {
+        if line.is_empty() {
+            max_cals = max(max_cals, current_cals);
+            current_cals = 0;
+        } else {
+            current_cals += line.parse::<u32>().unwrap();
+        }
+    }
+    println!("{}", max_cals);
+}
+
+fn part_two(file_contents: &str) {
+    let mut calories = BTreeSet::new();
+    let mut current_cals: u32 = 0;
+    for line in file_contents.lines() {
+        if line.is_empty() {
+            calories.insert(current_cals);
+            current_cals = 0;
+        } else {
+            current_cals += line.parse::<u32>().unwrap();
+        }
+    }
+    let total_top_three: u32 = calories.iter().rev().take(3).sum();
+    println!("{}", total_top_three);
+}
+
