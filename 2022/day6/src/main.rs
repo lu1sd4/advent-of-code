e88a892d69be6efcf88dab7b2722ba61ce5d2f14
lu1sd4 <luisdaniel.ld24@gmail.com>
2022-12-07 18:51:52 +0100

    Update day 6

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -13,11 +13,11 @@ fn is_marker(sequence: &[char]) -> bool {
     return set.len() == sequence.len();
 }
 
-fn part_one(file_contents: &str) {
+fn solve_with_window_size(file_contents: &str, window_size: &usize) {
     for line in file_contents.lines() {
         let message: Vec<char> = line.chars().collect();
         let mut i_left: usize = 0;
-        let mut i_right: usize = 4;
+        let mut i_right: usize = *window_size;
         let mut window = &message[i_left .. i_right];
         while !is_marker(window) {
             i_left += 1;
@@ -28,17 +28,10 @@ fn part_one(file_contents: &str) {
     }
 }
 
+fn part_one(file_contents: &str) {
+    solve_with_window_size(file_contents, &4);
+}
+
 fn part_two(file_contents: &str) {
-    for line in file_contents.lines() {
-        let message: Vec<char> = line.chars().collect();
-        let mut i_left: usize = 0;
-        let mut i_right: usize = 14;
-        let mut window = &message[i_left .. i_right];
-        while !is_marker(window) {
-            i_left += 1;
-            i_right += 1;
-            window = &message[i_left .. i_right];
-        }
-        println!("{i_right}");
-    }
+    solve_with_window_size(file_contents, &14);
 }
